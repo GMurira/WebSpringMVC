5d804a32417dc0aa4d5e287dabbf4c6115c65cde
GMurira <nguyaimurira@gmail.com>
2026-02-09 19:29:28 +0300

    Working with jdbc

--- src/main/java/dev/murira/runner/controller/RunController.java
+++ src/main/java/dev/murira/runner/controller/RunController.java
@@ -20,43 +20,44 @@ public class RunController {
         this.runRepository = runRepository;
     }
 
+    //
     @GetMapping("")
     List<Run> findAll() {
-        return runRepository.getAllRuns();
+        return runRepository.findAll();
     }
-
-    @GetMapping("/{id}")
-    Run findById(@PathVariable Integer id) {
-        Optional<Run> run = runRepository.findById(id);
-        if (run.isEmpty()) {
-            throw new RunNotFoundException();
-        }
-        return run.get();
-    }
-
-    @GetMapping("hello")
-    String helloRunner() {
-        return "We home you have a nice run";
-    }
-
-    //Post
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("")
-    void create(@Valid @RequestBody Run run) {
-        runRepository.create(run);
-    }
-
-    //put
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PutMapping("/{id}")
-    void update(@RequestBody Run run, @PathVariable Integer id) {
-        runRepository.update(run, id);
-    }
-
-    //delete
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/{id}")
-    void delete(@PathVariable Integer id) {
-        runRepository.delete(id);
-    }
+//
+//    @GetMapping("/{id}")
+//    Run findById(@PathVariable Integer id) {
+//        Optional<Run> run = runRepository.findById(id);
+//        if (run.isEmpty()) {
+//            throw new RunNotFoundException();
+//        }
+//        return run.get();
+//    }
+//
+//    @GetMapping("hello")
+//    String helloRunner() {
+//        return "We home you have a nice run";
+//    }
+//
+//    //Post
+//    @ResponseStatus(HttpStatus.CREATED)
+//    @PostMapping("")
+//    void create(@Valid @RequestBody Run run) {
+//        runRepository.create(run);
+//    }
+//
+//    //put
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    @PutMapping("/{id}")
+//    void update(@RequestBody Run run, @PathVariable Integer id) {
+//        runRepository.update(run, id);
+//    }
+//
+//    //delete
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    @DeleteMapping("/{id}")
+//    void delete(@PathVariable Integer id) {
+//        runRepository.delete(id);
+//    }
 }
--- src/main/java/dev/murira/runner/controller/RunRepository.java
+++ src/main/java/dev/murira/runner/controller/RunRepository.java
@@ -1,63 +1,46 @@
 package dev.murira.runner.controller;
 
 import dev.murira.runner.run.Run;
-import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @Repository
 public class RunRepository {
-    private List<Run> runs = new ArrayList<>();
+    private static final Logger logger = LoggerFactory.getLogger(RunRepository.class);
+    private final JdbcClient jdbcClient;
 
-    public List<Run> getAllRuns() {
-        return runs;
+
+    public RunRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
     }
 
-    Optional<Run> findById(Integer id) {
-        return runs.stream()
-                .filter(run -> Objects.equals(run.id(), id))
-                .findFirst();
+    public List<Run> findAll() {
+        return jdbcClient.sql("SELECT * FROM run")
+                .query(Run.class)
+                .list();
     }
 
-    //create a run
-    void create(Run run) {
-        runs.add(run);
+    public void create(Run run) {
+        var updated = jdbcClient.sql("INSERT INTO run(id, title, started_on,completed_on)values (?,?,?,?)")
+                .params(List.of(run.id(), run.title(), run.startedOn(), run.completedOn().toString()))
+                .update();
+
+        Assert.state(updated == 1, "Failed to create run" + run.title());
     }
 
-    //update
-    void update(Run run, Integer id) {
-        Optional<Run> existingRun = findById(id);
-        if (existingRun.isPresent()) {
-            runs.set(runs.indexOf(existingRun.get()), run);
-        }
+    public Optional<Run> findById(Integer id){
+        return jdbcClient.sql("SELECT id, title, started_on,completed_on FROM run WHERE id = :id")
+                .param("id",id)
+                .query(Run.class)
+                .optional();
     }
 
-
-    //delete
-    void delete(Integer id) {
-        runs.removeIf(run -> run.id().equals(id));
-    }
-
-    @PostConstruct
-    private void init() {
-        runs.add(new Run(
-                1,
-                "Tuesday run",
-                LocalDateTime.now(),
-                LocalDateTime.now().plus(1, ChronoUnit.HOURS)
-        ));
-
-        runs.add(new Run(
-                2,
-                "Wednesday run",
-                LocalDateTime.now(),
-                LocalDateTime.now().plus(2, ChronoUnit.HOURS)
-        ));
-    }
+    public
 }
