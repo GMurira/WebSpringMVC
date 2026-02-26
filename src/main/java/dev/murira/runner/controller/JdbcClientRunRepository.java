418e6d52c948a40c83fadceab4062b44ce29254d
GMurira <nguyaimurira@gmail.com>
2026-02-26 12:07:08 +0300

    Added Spring JPA

---
+++ src/main/java/dev/murira/runner/controller/JdbcClientRunRepository.java
@@ -0,0 +1,61 @@
+package dev.murira.runner.controller;
+
+import dev.murira.runner.run.Run;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class RunRepository {
+    private static final Logger logger = LoggerFactory.getLogger(RunRepository.class);
+    private final JdbcClient jdbcClient;
+
+
+    public RunRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+
+    public List<Run> findAll() {
+        return jdbcClient.sql("SELECT * FROM run")
+                .query(Run.class)
+                .list();
+    }
+
+    public void create(Run run) {
+        var updated = jdbcClient.sql("INSERT INTO run(id, title, started_on,completed_on)values (?,?,?,?)")
+                .params(List.of(run.id(), run.title(), Timestamp.valueOf(run.startedOn()), Timestamp.valueOf(run.completedOn())))
+                .update();
+
+        Assert.state(updated == 1, "Failed to create run" + run.title());
+    }
+
+    public Optional<Run> findById(Integer id) {
+        return jdbcClient.sql("SELECT id, title, started_on,completed_on FROM run WHERE id = :id")
+                .param("id", id)
+                .query(Run.class)
+                .optional();
+    }
+
+    public void delete(Integer id) {
+        var updated = jdbcClient.sql("DELETE FROM run WHERE id = :id ")
+                .param("id", id)
+                .update();
+        if (updated == 0) {
+            throw new RuntimeException("Employee not found with id " + id);
+        }
+    }
+
+    public void saveAll(List<Run> runs) {
+        runs.stream().forEach(this::create);
+    }
+
+    public Integer count() {
+        return jdbcClient.sql("SELECT * FROM run").query().listOfRows().size();
+    }
+}
